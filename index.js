f404ff985d54f6751f07bc552ef4e99168de6933
Abasoff <anar.abasov24@gmail.com>
2026-04-05 02:06:50 +0300

    getBet fonksiyonu eklendi

--- index.js
+++ index.js
@@ -28,7 +28,24 @@ const getNumberOfLines = () => {
     }
 }
 
+const getBet = (balance) => {
+    while(true){
+        const bet = prompt("Enter the total bet: ");
+        const numberBet = parseFloat(Lines);
 
-let balance = setDeposit();
+        if(isNaN(numberBet) || numberBet <= 0 || numberBet >= balance){
+            console.log("Invalid bet,Try again");
+        }
+        else{
+            return numberBet;
+        }
+    }
+}
+
+
+let callBalance = setDeposit();
 const numberOfLines = getNumberOfLines();
+const callBet = getBet();
+console.log(callBalance);
 console.log(numberOfLines);
+console.log(callBet);
